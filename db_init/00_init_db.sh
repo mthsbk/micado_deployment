ca63b8727c18bded226260cf3a46208243b2d440
Luca Gioppo <gioppoluca@libero.it>
2020-03-24 15:41:11 +0100

    refactor api manager and identity to take into account the split of the DB schemas

--- db_init/00_init_db.sh
+++ db_init/00_init_db.sh
@@ -21,27 +21,21 @@ echo "\nCreated MICADO apps components, now adding tables\n"
 
 psql -U $MICADO_DB_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/01_db.sql.txt
 
+# ---------- API MANAGER SHARED DB
 
-echo "\nCreating WSO2_Identity components\n"
-psql -c "CREATE USER $WSO2_IDENTITY_USER WITH ENCRYPTED PASSWORD '$WSO2_IDENTITY_PWD';"
-psql -c "CREATE SCHEMA $WSO2_IDENTITY_SCHEMA;"
-psql -c "GRANT CONNECT ON DATABASE $POSTGRES_DB TO $WSO2_IDENTITY_USER;"
-psql -c "GRANT USAGE ON SCHEMA $WSO2_IDENTITY_SCHEMA TO $WSO2_IDENTITY_USER;"
-psql -c "GRANT CREATE ON SCHEMA $WSO2_IDENTITY_SCHEMA TO $WSO2_IDENTITY_USER;"
-psql -c "ALTER ROLE $WSO2_IDENTITY_USER IN DATABASE $POSTGRES_DB SET search_path = $WSO2_IDENTITY_SCHEMA;"
-psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA $WSO2_IDENTITY_SCHEMA GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO $WSO2_IDENTITY_USER;"
-psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA $WSO2_IDENTITY_SCHEMA GRANT USAGE ON SEQUENCES TO $WSO2_IDENTITY_USER;"
+echo "\nCreating WSO2_Shared components\n"
+psql -c "CREATE USER $WSO2_SHARED_USER WITH ENCRYPTED PASSWORD '$WSO2_SHARED_PWD';"
+psql -c "CREATE SCHEMA $WSO2_SHARED_SCHEMA;"
+psql -c "GRANT CONNECT ON DATABASE $POSTGRES_DB TO $WSO2_SHARED_USER;"
+psql -c "GRANT USAGE ON SCHEMA $WSO2_SHARED_SCHEMA TO $WSO2_SHARED_USER;"
+psql -c "GRANT CREATE ON SCHEMA $WSO2_SHARED_SCHEMA TO $WSO2_SHARED_USER;"
+psql -c "ALTER ROLE $WSO2_SHARED_USER IN DATABASE $POSTGRES_DB SET search_path = $WSO2_SHARED_SCHEMA;"
+psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA $WSO2_SHARED_SCHEMA GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO $WSO2_SHARED_USER;"
+psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA $WSO2_SHARED_SCHEMA GRANT USAGE ON SEQUENCES TO $WSO2_SHARED_USER;"
 
-echo "\nCreated WSO2_IDENTITY components, now adding tables\n"
+echo "\nCreated WSO2_SHARED components, now adding tables\n"
 
-psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/03_wso2_is_postgresql.sql.txt
-psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/04_wso2_is_api_postgresql_merged.sql.txt
-psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/05_wso2_is_identity_uma_postgresql.sql.txt
-psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/06_wso2_is_postgresql_11-tokencleanup.sql.txt
-psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/07_wso2_is_postgresql-tokencleanup-restore.sql.txt
-psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/09_wso2_is_bpel_postgresql.sql.txt
-psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/11_wso2_is_metric_postgresql.sql.txt
-
+psql -U $WSO2_SHARED_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/api_shared_postgresql.sql.txt
 
 echo "\nCreating WSO2_API components\n"
 psql -c "CREATE USER $WSO2_API_USER WITH ENCRYPTED PASSWORD '$WSO2_API_PWD';"
@@ -55,11 +49,34 @@ psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA $WSO2_API_SCHEMA GRANT USAGE ON SEQU
 
 echo "\nCreated WSO2_API components, now adding tables\n"
 
+psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/api_api_postgresql.sql.txt
+#psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/12_wso2_api_postgresql.sql.txt
+#psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/13_wso2_api_apim_postgresql.sql.txt
+#psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/14_wso2_api_postgresql-mb.sql.txt
+#psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/15_wso2_api_metrics_postgresql.sql.txt
 
-psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/12_wso2_api_postgresql.sql.txt
-psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/13_wso2_api_apim_postgresql.sql.txt
-psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/14_wso2_api_postgresql-mb.sql.txt
-psql -U $WSO2_API_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/15_wso2_api_metrics_postgresql.sql.txt
+
+#echo "\nCreating WSO2_Identity components\n"
+#psql -c "CREATE USER $WSO2_IDENTITY_USER WITH ENCRYPTED PASSWORD '$WSO2_IDENTITY_PWD';"
+#psql -c "CREATE SCHEMA $WSO2_IDENTITY_SCHEMA;"
+#psql -c "GRANT CONNECT ON DATABASE $POSTGRES_DB TO $WSO2_IDENTITY_USER;"
+#psql -c "GRANT USAGE ON SCHEMA $WSO2_IDENTITY_SCHEMA TO $WSO2_IDENTITY_USER;"
+#psql -c "GRANT CREATE ON SCHEMA $WSO2_IDENTITY_SCHEMA TO $WSO2_IDENTITY_USER;"
+#psql -c "ALTER ROLE $WSO2_IDENTITY_USER IN DATABASE $POSTGRES_DB SET search_path = $WSO2_IDENTITY_SCHEMA;"
+#psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA $WSO2_IDENTITY_SCHEMA GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO $WSO2_IDENTITY_USER;"
+#psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA $WSO2_IDENTITY_SCHEMA GRANT USAGE ON SEQUENCES TO $WSO2_IDENTITY_USER;"
+
+#echo "\nCreated WSO2_IDENTITY components, now adding tables\n"
+
+#psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/03_wso2_is_postgresql.sql.txt
+#psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/04_wso2_is_api_postgresql_merged.sql.txt
+#psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/05_wso2_is_identity_uma_postgresql.sql.txt
+#psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/06_wso2_is_postgresql_11-tokencleanup.sql.txt
+#psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/07_wso2_is_postgresql-tokencleanup-restore.sql.txt
+#psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/09_wso2_is_bpel_postgresql.sql.txt
+#psql -U $WSO2_IDENTITY_USER -d $POSTGRES_DB -a -q -f /docker-entrypoint-initdb.d/11_wso2_is_metric_postgresql.sql.txt
+
+
 
 echo "\nCreating WEBLATE components\n"
 psql -c "CREATE USER $WEBLATE_POSTGRES_USER WITH ENCRYPTED PASSWORD '$WEBLATE_POSTGRES_PASSWORD';"
