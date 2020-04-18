bcb56a7ba26b2274fbc34c618cccf02f12326fb5
Luca Gioppo <gioppoluca@libero.it>
2020-04-18 14:45:58 +0200

    honing the start script

--- start_env_prod.sh
+++ start_env_prod.sh
@@ -12,17 +12,23 @@ docker-compose -f docker-compose.yml up -d countly_api countly_frontend identity
 sleep 60s
 
 echo 'Now starting applications'
-docker-compose -f docker-compose.yml up -d nginx data_migrants data_pa watchtower
+docker-compose -f docker-compose.yml up -d nginx data_migrants data_pa data_ngo watchtower
+
+sleep 60s
 
 echo 'not started at the moment the translation system, db management, chatbot, upload_service, dashboard'
 echo 'if you want to start postgresql db management use:'
 echo 'docker-compose -f docker-compose.yml up -d db_admin'
+docker-compose -f docker-compose.yml up -d db_admin
+
 echo 'if you want to start mongo db management use:'
 echo 'docker-compose -f docker-compose.yml up -d mongo-express'
 
 echo 'if you want to start translation system:'
 echo 'docker-compose -f docker-compose.yml up -d git weblate cache'
+docker-compose -f docker-compose.yml up -d git weblate cache
 
 echo 'if you want to start dashboard:'
 echo 'docker-compose -f docker-compose.yml up -d dashboard'
+docker-compose -f docker-compose.yml up -d dashboard
 
